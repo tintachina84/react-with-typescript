13648db92ccc04d61e3edab2b9f2e57804bc50c9
seonghyeon.seo <tintachina84@gmail.com>
2024-09-10 17:32:32 +0900

    Added: react-router-dom with todo app.

---
+++ react-router-todo/src/pages/DataView/index.tsx
@@ -0,0 +1,27 @@
+import styled from '@emotion/styled';
+import { Title } from 'components/Title';
+import { ToDoList } from 'components/ToDoList';
+import { ShowInputButton } from '../ShowInputButton';
+import { useNavigate } from 'react-router-dom';
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  background-color: #fff;
+  padding: 32px;
+  border-radius: 8px;
+`;
+
+export const DataView = () => {
+  const navigate = useNavigate();
+
+  return (
+    <Container>
+      <Title label="Todo list" />
+      <ToDoList />
+      <ShowInputButton show={false} onClick={() => navigate('/add')} />
+    </Container>
+  );
+};
