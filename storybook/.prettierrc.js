ed3c473a11833000cad1355af8dc41a2dc6c6bb8
seonghyeon.seo <tintachina84@gmail.com>
2024-09-24 09:31:34 +0900

    Added: storybook.

---
+++ storybook/.prettierrc.js
@@ -0,0 +1,9 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+  singleQuote: true,
+  semi: true,
+  useTabs: false,
+  trailingComma: 'all',
+  printWidth: 120,
+  arrowParens: 'avoid',
+};
---
+++ storybook/eslint.config.mjs
@@ -0,0 +1,40 @@
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import pluginReact from 'eslint-plugin-react';
+
+export default [
+  {
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+  },
+  { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'] },
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  {
+    rules: {
+      // "no-unused-vars": "error",
+      // "no-undef": "error",
+      'react/jsx-uses-react': 'off',
+      'react/react-in-jsx-scope': 'off',
+    },
+  },
+  {
+    ignores: [
+      'cypress',
+      'node_modules',
+      'public',
+      'src/assets',
+      '.gitignore',
+      '.prettierrc',
+      '.prettierignore',
+      'cypress.config.js',
+      'eslint.config.mjs',
+    ],
+  },
+];
